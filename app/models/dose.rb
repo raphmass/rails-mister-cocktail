2619bd55dcafb9a47cc229bd462dd0438e09a0d3
raphael massonneau <raphael.massonneau@gmail.com>
2019-05-17 15:23:10 +0200

    clean back-end code

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,6 +1,5 @@
 class Dose < ApplicationRecord
-  validates :description, presence: true
   belongs_to :cocktail
   belongs_to :ingredient
-  validates :ingredient_id, :uniqueness => { :scope => :cocktail_id }
+  validates :cocktail, uniqueness: { scope: :ingredient }
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -3,3 +3,4 @@ class Ingredient < ApplicationRecord
 	has_many :cocktails, through: :doses
 	validates :name, uniqueness: true, presence: true
 end
+
